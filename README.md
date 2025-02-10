bc4ef2dcb4d6b2bfbe9a24baea1b568ec3299fd7
Andrew Richard <acrichard07@gmail.com>
2025-02-09 16:42:14 -0800

    readme updated, app runs, some errors in data input thought

--- README.md
+++ README.md
@@ -7,3 +7,18 @@ takes inputs of date (ISO 8601 standard) and weight (choose units, but currently
 
 ## outputs
 outputs three plots: one for each cat, and one combined with both cats plotted by the age they were in weeks, not by date.
+
+## some shiny for python tips
+To run a Shiny app from the command line, use the shiny run command. This command takes a single argument, the path to the app’s entry point. For example, if your app’s entry point is app.py in the directory ./app_dir, you can run it like this:
+
+
+`shiny run --reload --launch-browser app_dir/app.py`
+`run shiny app from cl in app directory w/ ‘shiny run app.py'`
+
+
+This should start your app and also automatically launch a web browser.
+
+
+The --reload flag means that file changes in the current directory tree will cause the Python process to restart and the browser to reload. Update and save changes to app.py and then wait a moment for the changes to appear in the browser.
+
+ctrl + c will interrupt and close your running app.
--- code/dashboard/app.py
+++ code/dashboard/app.py
@@ -3,7 +3,7 @@ from faicons import icon_svg
 import plotly.express as px
 
 # Import data from shared.py
-from shared import app_dir, df
+from shared import app_dir, df, weights
 
 from shiny import App, reactive, render, ui
 
@@ -12,10 +12,9 @@ app_ui = ui.page_sidebar(
 # toggle Sullivan, Haruki, or Both
     ui.sidebar(
         # delete slider input
-        ui.input_slider("mass", "Mass", 2000, 6000, 6000),
         ui.input_checkbox_group(
 # change "species" to better label, remmber to change in server
-            "species",
+            "cat",
             "Choose cat",
             ["Haruki", "Sullivan"],
             selected=["Haruki", "Sullivan"],
@@ -24,26 +23,26 @@ app_ui = ui.page_sidebar(
     ),
     ui.layout_column_wrap(
         ui.value_box(
-            "Sullivan's current age"
-            ui.output_text("count"),
-            showcase=icon_svg("earlybirds"),
+            "Sullivan's current age",
+            ui.output_text("sullivan_age"),
+            showcase=icon_svg("cat"),
         ),
         ui.value_box(
             "Haruki's current age",
-            ui.output_text("bill_length"),
-            showcase=icon_svg("ruler-horizontal"),
+            ui.output_text("haruki_age"),
+            showcase=icon_svg("cat"),
         ),
         ui.value_box(
             "Average growth rate",
-            ui.output_text("bill_depth"),
-            showcase=icon_svg("ruler-vertical"),
+            ui.output_text("growth_rate"),
+            showcase=icon_svg("percent"),
         ),
         fill=False,
     ),
     ui.layout_columns(
         ui.card(
             ui.card_header("Sullivan and Haruki's weight change"),
-            ui.output_plot("length_depth"),
+            ui.output_plot("weight_plot"),
             full_screen=True,
         ),
         ui.card(
@@ -61,39 +60,36 @@ app_ui = ui.page_sidebar(
 def server(input, output, session):
     @reactive.calc
     def filtered_df():
-        filt_df = df[df["species"].isin(input.species())]
-        filt_df = filt_df.loc[filt_df["body_mass_g"] < input.mass()]
+        filt_df = weights[weights["cat"].isin(input.cat())]
         return filt_df
 
     @render.text
-    def count():
+    def sullivan_age():
         return filtered_df().shape[0]
 
     @render.text
-    def bill_length():
-        return f"{filtered_df()['bill_length_mm'].mean():.1f} mm"
+    def haruki_age():
+        return f"{filtered_df()['weight']} lbs"
 
     @render.text
-    def bill_depth():
+    def growth_rate():
         return f"{filtered_df()['bill_depth_mm'].mean():.1f} mm"
 
     @render.plot
-    def length_depth():
-        return sns.scatterplot(
+    def weight_plot():
+        return px.scatter(
             data=filtered_df(),
-            x="bill_length_mm",
-            y="bill_depth_mm",
-            hue="species",
+            x="date",
+            y="weight",
+            hue="cat",
         )
 
     @render.data_frame
     def summary_statistics():
         cols = [
-            "species",
-            "island",
-            "bill_length_mm",
-            "bill_depth_mm",
-            "body_mass_g",
+            "cat",
+            "date",
+            "weight",
         ]
         return render.DataGrid(filtered_df()[cols], filters=True)
 
